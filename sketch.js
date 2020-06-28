42107801483631ad1d68a30d0d2c3abd0c464e3f
aarushvasanadu <sritvidya@yahoo.com>
2020-06-28 11:45:02 +0530

    constraint

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -30,13 +31,15 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
+    log6 = new Log(200,200,100, PI/2)
+    
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -59,8 +62,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
     platform1.display();
+    chain.display();
 }
